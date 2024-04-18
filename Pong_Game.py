3af949f1df662df0d50b30a640818ed81090f5f6
Andrey Svistunov <11free11@mail.ru>
2024-04-18 21:44:00 +0300

    Animation player of right side

--- Pong_Game.py
+++ Pong_Game.py
@@ -1,5 +1,17 @@
-import sys
 import pygame
+import sys
+
+
+def animate_ball():
+    global ball_speed_x, ball_speed_y
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.bottom >= screen_hight or ball.top <= 0:
+        ball_speed_y *= -1
+    if ball.right >= screen_width or ball.left <= 0:
+        ball_speed_x *= -1
+
 
 pygame.init()
 
@@ -19,32 +31,37 @@ cpu.centery = screen_hight/2
 player = pygame.Rect(0, 0, 20, 100)
 player.midright = (screen_width, screen_hight/2)
 
-ball_spped_x = 6
-ball_spped_y = 6
+ball_speed_x = 6
+ball_speed_y = 6
+player_speed = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player_speed = -6
+            if event.key == pygame.K_DOWN:
+                player_speed = 6
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                player_speed = 0
+            if event.key == pygame.K_DOWN:
+                player_speed = 0
 
     # Change the position of the game objects
-    ball.x += ball_spped_x
-    ball.y += ball_spped_y
+    animate_ball()
+    player.y += player_speed
 
-    if ball.bottom >= screen_hight or ball.top <= 0:
-        ball_spped_y *=  -1
-    if ball.right >= screen_width or ball.left <=0:
-        ball_spped_x *= -1
-
-
-    # Draw the game object / Игровой объект
+    # Draw the game object Игровой объект
     screen.fill('black')
     pygame.draw.aaline(screen, 'white', (screen_width/2, 0), (screen_width/2, screen_hight))
     pygame.draw.ellipse(screen, 'white', ball)
     pygame.draw.rect(screen, 'white', cpu)
     pygame.draw.rect(screen, 'white', player)
 
-    # Update the display / обновление экрана
+    # Update the display обновление экрана
     pygame.display.update()
     clock.tick(60)
